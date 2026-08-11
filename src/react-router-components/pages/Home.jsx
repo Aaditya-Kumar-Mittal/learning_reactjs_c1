9161dfbce045304be0b4d0a2f56afc89b2d89ecb
Aaditya Kumar Mittal <adityakumaransh2007@gmail.com>
2026-08-12 00:23:13 +0530

    feat: Setting up Tailwindcss in ReactJS

--- src/react-router-components/pages/Home.jsx
+++ src/react-router-components/pages/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+    </div>
   )
 }
 
